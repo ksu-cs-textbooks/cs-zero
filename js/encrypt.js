41ff7e2ccc552b1147e053caec33470bdaefd8f6
russfeld <russfeld@users.noreply.github.com>
2023-08-23 19:21:02 +0000

    deploy: 0e7e0da248992222098c15f7f309dbb8748887ea

--- js/encrypt.js
+++ js/encrypt.js
@@ -1,30 +1,27 @@
 // [x] russfeld
-document.addEventListener("DOMContentLoaded", function() {
-
-  document.getElementById("encrypt")?.addEventListener("click", function (event) {
+$(document).ready(function(){
+  $('#encrypt').click(function(event){
     event.preventDefault();
     var password = prompt("Please enter the password");
     if (password) {
-      var div = document.getElementById("encrypt-div").innerHTML;
+      var div = $('#encrypt-div').html();
       var encrypted = sjcl.encrypt(password, div);
       var encryptedJSON = JSON.stringify(encrypted);
       var base64 = btoa(encryptedJSON);
-      document.getElementById("encrypt").style.display = "none";
-      document.getElementById("encrypt-div").innerHTML = "<p>" + base64 + "</p>";
+      $('#encrypt').hide();
+      $('#encrypt-div').html("<p>" + base64 + "</p>");
     }
   });
-
-  document.getElementById("decrypt")?.addEventListener("click", function (event) {
+  $('#decrypt').click(function(event){
     event.preventDefault();
     var password = prompt("Please enter the password");
     if (password) {
-      var div = document.getElementById("encrypt-div").innerText;
-      console.log(div);
+      var div = $('#encrypt-div').text();
       var encryptedJSON = atob(div);
       var encrypted = JSON.parse(encryptedJSON);
       var decrypted = sjcl.decrypt(password, encrypted);
-      document.getElementById("decrypt").style.display = "none";
-      document.getElementById("encrypt-div").innerHTML = "<p>" + decrypted + "</p>";
+      $('#encrypt').hide();
+      $('#encrypt-div').html(decrypted);
     }
   });
 });
